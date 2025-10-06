760e44ea425acb90b88b4a257254a8f87142764a
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2025-10-06 10:47:13 +0200

    stötte in på problem elev vill inte resolve i main

--- src/elev.java
+++ src/elev.java
@@ -1,8 +1,16 @@
 public class elev {
     private String name;
-    private String  email;
+    private String email;
     private String klass;
     private String gymnasielinje;
     private String personnummer;
+
+    public void Elev(String name,String email, String klass, String gymnasielinje,String personnummer){
+        this.name = name;
+        this.email=email;
+        this.klass=klass;
+        this.gymnasielinje=gymnasielinje;
+        this.personnummer=personnummer;
+    }
 
 }
